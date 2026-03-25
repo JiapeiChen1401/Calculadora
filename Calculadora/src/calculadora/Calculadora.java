9531a0a9861649f2c8c0447dc43038b8bb15839f
ALUMNO <jche1401@g.educaand.es>
2026-03-25 13:26:33 +0100

    Commit hotfix

--- Calculadora/src/calculadora/Calculadora.java
+++ Calculadora/src/calculadora/Calculadora.java
@@ -49,6 +49,9 @@ public class Calculadora {
 	 * @return Cociente de a/b.
 	 */
 	public double dividir(int a, int b) {
+		if(b==0) {
+			throw new IllegalArgumentException("No se puede dividir entre cero");
+		}
 		return (double)a/b;
 	}
 }
