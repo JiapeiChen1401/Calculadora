2e122e24a8dd1feac2fb3bd63ccea10c1b5d80d9
ALUMNO <jche1401@g.educaand.es>
2026-03-25 13:05:06 +0100

    Commit release

--- Calculadora/src/calculadora/Calculadora.java
+++ Calculadora/src/calculadora/Calculadora.java
@@ -1,18 +1,53 @@
 package calculadora;
 
+/**
+ * Conjunto de operaciones básicas (sumar, restar, multiplicar y dividir).
+ * 
+ * @author Jiapei
+ * @version 1.0.0
+ * @since 2026
+ */
 public class Calculadora {
+	/**
+	 * Este método devuelve la suma de dos números enteros.
+	 * 
+	 * @param a Primer sumando.
+	 * @param b Segundo sumando.
+	 * @return Resultado de la suma de a+b.
+	 */
 	public int sumar(int a, int b) {
 		return a+b; 
 	}
 	
+	/**
+	 * Este método devuelve la diferencia entre dos números enteros.
+	 * 
+	 * @param a Minuendo.
+	 * @param b Sustraendo.
+	 * @return Resultado de la resta de a-b.
+	 */
 	public int restar(int a, int b) {
 		return a-b; 
 	}
 	
+	/**
+	 * Este método devuelve el producto de dos números enteros.
+	 * 
+	 * @param a Multiplicando.
+	 * @param b Multiplicador.
+	 * @return Producto de a*b.
+	 */
 	public int multiplicar(int a, int b) {
 		return a*b; 
 	}
 	
+	/**
+	 * Este médoto devuelve el cociente de de la división entre dos números.
+	 * 
+	 * @param a Dividendo.
+	 * @param b Divisor.
+	 * @return Cociente de a/b.
+	 */
 	public double dividir(int a, int b) {
 		return (double)a/b;
 	}
